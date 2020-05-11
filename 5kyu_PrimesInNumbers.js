c17116a0d56083228a8068706ff2167d0c5be3f4
shavla <57368636+shavla@users.noreply.github.com>
2020-05-11 17:02:25 +0400

    Solved

---
+++ 5kyu_PrimesInNumbers.js
@@ -0,0 +1,22 @@
+function primeFactors(n){
+  let dic = {};
+  for (let i = 2; i <= n; i++)
+  {
+    while (n % i === 0)
+    {
+      if (!(i in dic))
+         dic[i] = 0;
+      dic[i] += 1;
+      n = Math.floor(n / i);
+    }
+  }
+  let res = '';
+  for (let i in dic)
+  {
+      if (dic[i] > 1)
+        res += '(' + i + '**' + dic[i] + ')';
+      else
+        res += '(' + i + ')';
+  }
+  return res;
+}
