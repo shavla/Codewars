7893ee59fc755083b07e79c3bc1159860c107445
shavla <57368636+shavla@users.noreply.github.com>
2020-05-12 12:20:47 +0400

    Solved

---
+++ 6kyu_UniqueInOrder.js
@@ -0,0 +1,9 @@
+var uniqueInOrder=function(iterable){
+  let arr = [];
+  for(let i = 0; i <= iterable.length; i++){
+    if(iterable[i] != iterable[i + 1]){
+       arr.push(iterable[i]);
+    }
+  }
+  return arr;
+}
