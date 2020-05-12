84cf47b011d4dc4027214b86fab141620256c7bb
shavla <57368636+shavla@users.noreply.github.com>
2020-05-12 11:04:05 +0400

    Solved

---
+++ 7kyu_FindTheDivisors!.js
@@ -0,0 +1,8 @@
+function divisors(integer) {
+  let arr = [];
+  for(let i = 2; i < integer; i++){
+    if(integer % i == 0) arr.push(i);
+  }
+  if(arr.length == 0) return integer + " is prime";
+  return arr;
+};
