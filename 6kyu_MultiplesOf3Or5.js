bf38de0d765d2fdc68cfe2276a5c26ccbe640ece
shavla <57368636+shavla@users.noreply.github.com>
2020-05-12 12:53:24 +0400

    Solved

---
+++ 6kyu_MultiplesOf3Or5.js
@@ -0,0 +1,16 @@
+function solution(number){
+  let sum = 0;  
+  for(let i = 1; i < number; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+      sum += i;
+      continue;
+    }
+    if(i % 3 == 0){
+      sum += i;
+    }
+    if(i % 5 == 0){
+      sum += i;
+    }
+  }
+  return sum;
+}
